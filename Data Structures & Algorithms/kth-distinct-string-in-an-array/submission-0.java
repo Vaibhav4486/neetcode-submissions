f0c4c36aa7e15349c03019da477e8f70db04cfe5
Vaibhav4486 <149326437+Vaibhav4486@users.noreply.github.com>
2026-08-09 13:11:46 +0530

    Add: kth-distinct-string-in-an-array - submission-0

---
+++ Data Structures & Algorithms/kth-distinct-string-in-an-array/submission-0.java
@@ -0,0 +1,23 @@
+
+
+class Solution {
+    public String kthDistinct(String[] arr, int k) {
+        LinkedHashMap<String, Integer> countMap = new LinkedHashMap<>();
+        
+        for (String s : arr) {
+            countMap.put(s, countMap.getOrDefault(s, 0) + 1);
+        }
+        
+        int distinctCount = 0;
+        for (Map.Entry<String, Integer> entry : countMap.entrySet()) {
+            if (entry.getValue() == 1) {
+                distinctCount++;
+                if (distinctCount == k) {
+                    return entry.getKey();
+                }
+            }
+        }
+        
+        return "";
+    }
+}
